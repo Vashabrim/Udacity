4daf8afdd637fdea714b523e9f651f40ae1fa2b7
Vashabrim <vashabrim@gmail.com>
2015-06-25 10:35:56 -0500

    Still in Lesson1
    before finishing up objects

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -1,10 +1,60 @@
-var formattedName = HTMLheaderName.replace("%data%", "Doug Stubbs");
+//var formattedName=HTMLheaderName.replace("%data%", "Doug Stubbs");
 
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+//var role = "Web Developer";
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+//$("#header").prepend(formattedRole);
+//$("#header").prepend(formattedName);
 
-var skills = ["Awesomeness", "Web Development", "Desktop Support"];
-$("#main").append(skills);
+var bio = {
+	"name":"Doug",
+	"role":"Web Developer",
+	"contacts":{
+		"mobile":"515-491-9020",
+		"email":"stubbs.doug@gmail.com",
+		"github":"vashabrim",
+		"twitter":"@1TrueDAS",
+		"location":"Des Moines"
+	},
+	"welcomeMessage":"Bacon ipsum dolor amet id filet mignon dolore pork belly capicola kevin meatloaf dolore aliquip in. Anim picanha alcatra jowl corned beef ullamco voluptate magna et qui sausage dolor. Salami laborum beef ribs ham. Ut porchetta spare ribs, magna nisi labore cillum alcatra turducken. Kielbasa non pig alcatra beef sunt excepteur jowl cillum laborum do pastrami. Cillum alcatra do cow dolor tail culpa ribeye flank.",
+	"skills":["Awesomeness","Web Developer", "Desktop Support"],
+	"bioPic":"images/fry.jpg"
+}
+
+//$("#main").append(bio.name);
+
+//bio.city= "Des Moines, IA";
+
+var work = {
+
+}
+
+work.position="Senior Analyst";
+work.employer="Nationwide Insurance";
+work.city="Des Moines, IA";
+work.years="4 years";
+
+var education = {
+    "schools": [
+        {
+            "name": "Marshalltown Community College",
+            "city": "Marshalltown, IA",
+            "degree": "Associates",
+            "major": [
+                "General Studies"
+            ]
+        },
+        {
+            "name": "Des Moines Area Community College",
+            "city": "Des Moines, IA",
+            "degree": "Web Development certificate- Incomplete",
+            "major": [
+                "Web Development certificate"
+            ]
+        }
+    ]
+}
+
+
+//$("#main").append(work["position"]);
+//$         ("#main").append(education.school);
