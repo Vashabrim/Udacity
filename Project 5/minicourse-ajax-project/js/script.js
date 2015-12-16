3dd15a96e33195665f24f693c7d7debd0fb3d6c1
Vashabrim <vashabrim@gmail.com>
2015-12-15 22:37:34 -0600

    streetview

--- Project 5/minicourse-ajax-project/js/script.js
+++ Project 5/minicourse-ajax-project/js/script.js
@@ -14,7 +14,14 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    var street = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = street + ', ' + cityStr;
 
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    $body.append('<img class = "bgimg" src="' + streetviewUrl + '">');
     return false;
 };
 
