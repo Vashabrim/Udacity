69a516225543fa62861511ea778845648c8a1214
Vashabrim <vashabrim@gmail.com>
2015-12-15 22:47:23 -0600

    NY Times

--- Project 5/minicourse-ajax-project/js/script.js
+++ Project 5/minicourse-ajax-project/js/script.js
@@ -22,6 +22,19 @@ function loadData() {
 
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class = "bgimg" src="' + streetviewUrl + '">');
+
+    //NY Times stuffts
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9f6851444491fe0eecf86e1d8bfd723d:0:73785537'
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times articles about ' + cityStr);
+
+        articles = data.response.docs;
+        for (var i = 0; i <articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>'+'</li>');
+        };
+    })
+
     return false;
 };
 
