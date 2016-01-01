6fff8fb139de2868bda2714a403a418bff25d06c
Vashabrim <vashabrim@gmail.com>
2016-01-01 11:30:53 -0600

    fts

--- Project 5/minicourse-ajax-project/js/script.js
+++ Project 5/minicourse-ajax-project/js/script.js
@@ -25,17 +25,41 @@ function loadData() {
 
     //NY Times stuffts
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9f6851444491fe0eecf86e1d8bfd723d:0:73785537'
-    $.getJSON(nytimesUrl, function(data){
+    $.getJSON(nytimesUrl, function (data) {
         $nytHeaderElem.text('New York Times articles about ' + cityStr);
 
         articles = data.response.docs;
-        for (var i = 0; i <articles.length; i++) {
+        for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>'+'</li>');
-        };
+            $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>')
+        }
+    }).error(function (e) {
+        $nytHeaderElem.text('New York Times articles could not be loaded');
     })
 
+
+    //Wikipedia stuffts
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    var wikiTimeout = setTimeout(function () {
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url: wikiUrl,
+        datatype: "jsonp",
+        // jsonp: "callback",
+        success: function (response) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articlelist.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            }
+            clearTimeout(wikiTimeout);
+        }
+    });
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
