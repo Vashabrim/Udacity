72de2fa97a5fbdefc0178106261f17f0c7a777d9
Vashabrim <vashabrim@gmail.com>
2016-02-28 13:09:52 -0600

    Initial setup

---
+++ Project5-master/js/main.js
@@ -0,0 +1,42 @@
+/****  Model - set variables and location data  ****/
+
+var map;
+var infowindow;
+
+function initMap() {
+    var dtdsm = { lat: 41.585548, lng: -93.632818 };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: dtdsm,
+        zoom: 16
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: dtdsm,
+        radius: 2500,
+        type: ['hotel']
+    }, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
