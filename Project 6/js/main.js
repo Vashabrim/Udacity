1962bffb012651f00143bd812451884739743e75
Vashabrim <vashabrim@gmail.com>
2016-04-24 14:50:36 -0500

    Places added

--- Project 6/js/main.js
+++ Project 6/js/main.js
@@ -3,21 +3,38 @@
 var map;
 var infowindow;
 
+var dsmEats = [
+    { "name": "Zombie Burger + Drink Lab", "location": "300 E Grand Ave, Des Moines, Iowa", "lat": "41.590581", "lng": "-93.613274" },
+    { "name": "Fong's Pizza", "location": "223 4th Street, Des Moines, Iowa", "lat": "41.585894", "lng": "-93.621786" },
+    { "name": "Royal Mile", "location": "210 4th Street, Des Moines, Iowa", "lat": "41.583463", "lng": "-93.621968" },
+    { "name": "Django", "location": "210 10th Street, Des Moines, Iowa", "lat": "41.584339", "lng": "-93.629251" },
+    { "name": "Exile Brewing Company", "location": "1514 Walnut Street, Des Moines, Iowa", "lat": "41.583170", "lng": "-93.636655" }
+];
+
 function initMap() {
-    var dtdsm = { lat: 41.5890778, lng: -93.6613354 };
+    var dtdsm = { lat: 41.586828, lng: -93.623171 };
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: dtdsm,
         zoom: 16
     });
 
+    for (var i = 0; i < dsmEats.length; i++) {
+        Coordinates = new google.maps.LatLng(dsmEats[i].lat, dsmEats[i].lng);
+        dsmEats[i].marker = new google.maps.Marker({
+            position: Coordinates,
+            map: map,
+            title: dsmEats[i].name
+        });
+        dsmEats[i].marker.addListener('click', (function (api) {
+            return function () {
+                clickData(api);
+            };
+        })); dsmEats[i];
+    }
+
     infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: dtdsm,
-        radius: 2500,
-        type: ['food']
-    }, callback);
+    ko.applyBindings(new ViewModel());
 }
 
 function callback(results, status) {
