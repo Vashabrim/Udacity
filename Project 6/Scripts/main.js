c629df70e45d9cef4ad8d71376837ad7ea92119e
Vashabrim <vashabrim@gmail.com>
2016-04-28 21:42:51 -0500

    moar

--- Project 6/Scripts/main.js
+++ Project 6/Scripts/main.js
@@ -1,4 +1,5 @@
 /****  Model - set variables and location data  ****/
+// StreetviewAPI key AIzaSyC39CKK1lA0_pOaXo0t-ffppuK2JdVBkbg
 
 var map;
 var infowindow;
@@ -56,4 +57,39 @@ function createMarker(place) {
         infowindow.setContent(place.name);
         infowindow.open(map, this);
     });
+}
+function createContent(dsmEats) {
+    var popupData = "<h3>" + dsmEats.name + "</h3>" + "<div>" + dsmEats.location + "</div>"
+    return popupData;
+}
+
+function clickData(dsmEats) {
+    dsmEats.marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function () { dsmEats.marker.setAnimation(null); }, 1400);
+    var url = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=40.720032,-73.988354&fov=90&heading=235&pitch=10&key=AIzaSyC39CKK1lA0_pOaXo0t-ffppuK2JdVBkbg"
+}
+
+//Filter
+
+var ViewModel = function () {
+    var self = this;
+    self.filter = ko.observable("");
+    self.viewEats = ko.computed(function () {
+        var dsmEatsArr = [];
+        dsmEats.forEach(function (eats) {
+            if (dsmEats.name.toLowerCase().indexOf(self.filter().toLowerCase()) > -1) {
+                dsmEatsArr.push(eats);
+                dsmEats.marker.setVisible(true);
+            } else eats.marker.setVisible(false);
+        });
+        return dsmEatsArr;
+    });
+    self.select = function (parent) {
+        clickData(parent);
+    };
+};
+
+var mapLoadError = function () {
+    var mapDiv = $('#map');
+    mapDiv.append('<h1>Error. Google Maps was unable to load</h1>')
 }
